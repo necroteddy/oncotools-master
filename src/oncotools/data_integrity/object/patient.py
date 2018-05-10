cc144711c3dabd8ec17e19d3d7f26a2168be5efe
DanYuan96 <dy2014@gmail.com>
2018-05-09 20:57:07 -0400

    Update patient.py

--- src/oncotools/data_integrity/object/patient.py
+++ src/oncotools/data_integrity/object/patient.py
@@ -1,1 +1,11 @@
 class patient():
+    def __init__(self, key, PRQ, ROIQ):
+        self.ROIQ = ROIQ
+        self.PRQ = PRQ
+        self.PatientKey = key
+    
+    def getROIQ
+    
+    def getPRQ
+    
+    def getKey
