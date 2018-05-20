f89845c58f67ab44544933f32455adad5efd933f
necroteddy <37003131+necroteddy@users.noreply.github.com>
2018-05-20 16:58:52 -0400

    Update data_roi.py

--- oncotools/data_integrity/data/data_roi.py
+++ oncotools/data_integrity/data/data_roi.py
@@ -1,3 +1,6 @@
+from oncotools.data_integrity.data.Query_Module import Query_Module
+from oncotools.utils.query.regions_of_interest import RegionsOfInterestQueries
+
 class data_roi(Query_Module):
     def __init__(self):
         self.name = "Query_Data"
@@ -6,12 +9,12 @@ class data_roi(Query_Module):
 
     def get_data(dbase, ID):
         #initialize query classes
-        ROIQ = RegionsOfInterestQueries(self.dbase)
+        ROIQ = RegionsOfInterestQueries(dbase)
         roi_names = ROIQ.get_roi_names()
         data = []
         for name in roi_names:
             ROI_ID = ROIQ.get_id_by_patient_rep_id_name(ID, name)
             if ROI_ID is not None:
-                mask = ROQI.get_mask(ROI_ID)
+                mask = ROIQ.get_mask(ROI_ID)
                 data.append(mask)
         return data
