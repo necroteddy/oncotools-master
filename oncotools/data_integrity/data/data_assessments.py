89e8e19fd575361ce459a1701d81a87cd0bc0b79
necroteddy <37003131+necroteddy@users.noreply.github.com>
2018-05-20 16:55:28 -0400

    Update data_assessments.py

--- oncotools/data_integrity/data/data_assessments.py
+++ oncotools/data_integrity/data/data_assessments.py
@@ -1,10 +1,13 @@
+from oncotools.data_integrity.data.Query_Module import Query_Module
+from oncotools.utils.query.assessments import AssessmentsQueries
+
 class data_roi(Query_Module):
-    def __init__():
+    def __init__(self):
         self.name = "Query_Data"
         self.function = "Basic Data Query Class."
         self.description = {self.name: self.function}
 
     def get_data(dbase, ID):
         #initialize query classes
-        AQ = AssessmentsQueries(self.dbase)
+        AQ = AssessmentsQueries(dbase)
         return AQ.get_assessments(ID)
